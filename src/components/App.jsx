b6926aa2bca42b5a42f3569e99c56e3b2c227602
DanTiw <danishtiwari2204@gmail.com>
2024-02-20 22:38:35 +0530

    commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function App() {
+  return (
+    <div className="container">
+      <h2>focus</h2>
+      <form className="form">
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
